c378a052945b0d1405a712e0201e5fe9ed897716
thexerothermic <elijahbrian0@gmail.com>
2022-03-21 10:35:05 -0500

    ExceptionMiddlewareChange

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -34,6 +34,7 @@ namespace API.Middleware
                 var response = _env.IsDevelopment()
                     ? new ApiException(context.Response.StatusCode,ex.Message,ex.StackTrace?.ToString())
                     : new ApiException(context.Response.StatusCode, "Internal Server Error");
+                if (_env.IsProduction()) response = new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString());
                 var options = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
                 var json = JsonSerializer.Serialize(response, options);
                 await context.Response.WriteAsync(json);
